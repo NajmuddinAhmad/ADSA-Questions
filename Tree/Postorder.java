0f875cc4146b378347567545f4ed2bbaa8c7fb9b
Najmuddin Ahmad <98642530+NajmuddinAhmad@users.noreply.github.com>
2025-12-08 12:30:51 +0530

    Add files via upload

---
+++ Tree/Postorder.java
@@ -0,0 +1,41 @@
+// Leetcode 145. Binary Tree Postorder Traversal
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+public class Postorder {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        PostOrder(root , result);
+        return result;
+    }
+    public void PostOrder(TreeNode root , List<Integer> result){
+        if(root == null){
+            return;
+        }
+        PostOrder(root.left , result);
+        PostOrder(root.right , result);
+        result.add(root.val);
+        
+    }
+}
+}
---
+++ Tree/Preorder.java
@@ -0,0 +1,40 @@
+// Leetcode 144. Binary Tree Preorder Traversal
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+public class Preorder {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Preorder(root , result);
+        return result;
+    }
+    public void Preorder(TreeNode root , List<Integer> result){
+        if(root == null){
+            return;
+        }
+        result.add(root.val);
+        Preorder(root.left , result);
+        Preorder(root.right , result);
+        
+    }
+}
+}
