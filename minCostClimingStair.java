3607886a6ddd671693348135afb625822b9c9250
Najmuddin Ahmad <98642530+NajmuddinAhmad@users.noreply.github.com>
2025-12-24 23:07:15 +0530

    Add files via upload

---
+++ minCostClimingStair.java
@@ -0,0 +1,31 @@
+// 746. Min Cost Climbing Stairs
+
+import java.util.Arrays;
+
+public class minCostClimingStair {
+    class Solution {
+    int[] dp;
+    public int minCostClimbingStairs(int[] cost) {
+        dp = new int[cost.length];
+        Arrays.fill(dp, -1);
+
+        return Math.min(helper(0, cost), helper(1, cost));
+    }
+
+    public int helper(int idx, int[] cost) {
+        if (idx >= cost.length) {
+            return 0;
+        }
+        if (dp[idx] != -1) {
+            return dp[idx];
+        }
+
+        int first = cost[idx] + helper(idx + 1, cost);
+        int second = cost[idx] + helper(idx + 2, cost);
+
+        dp[idx] = Math.min(first, second);
+        return dp[idx];
+    }
+}
+
+}
